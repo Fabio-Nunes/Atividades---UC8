7b64bb284b37ae1008105b56313abfe5d8ab6842
Fabio-Nunes <fabionunesvp@gmail.com>
2022-10-14 16:46:40 -0300

    Initial commit

---
+++ Atividade4_UC8.js
@@ -0,0 +1,16 @@
+// Se o participante for maior de 18 anos, permitir o cadastro; senão, alertar que o cadastro não é permitido pela idade.]
+// Listar Participantes e Palestrantes por evento.
+// Enquanto a quantidade de participantes for inferior a 100, permitir cadastro; senão, alertar que o cadastro não será permitido por ter excedido o limite.
+
+var Idade = ("18")
+if (Idade < 18)
+        console.log ("Idade não Permitida")
+else
+        console.log ("Idade Permitida")
+
+let ListadeParticipantes = 100
+if (ListadeParticipantes <100)
+console.log ("Participante Cadastrado")
+
+else
+console.log ("Excedido o limite de Participantes")
